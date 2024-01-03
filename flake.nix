9616e92892c6a4ba24be09db4e4497527d153d22
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-01-03 17:00:40 +0100

    flake: init
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Utilities for working with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = import ./packages { inherit pkgs; };
+      }
+    );
+}
